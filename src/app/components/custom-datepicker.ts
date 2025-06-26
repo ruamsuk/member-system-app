b19ca268666f390de2746b4ed7213218c6ef5a66
ruamsuk <ruamsuk_w@hotmail.com>
2025-06-26 11:50:13 +0700

    First

---
+++ src/app/components/custom-datepicker.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-datepicker',
+  imports: [],
+  template: `
+    <p>
+      custom-datepicker works!
+    </p>
+  `,
+  styles: ``
+})
+export class CustomDatepicker {
+
+}
